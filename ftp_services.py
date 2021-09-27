ae9b1446702cd760cc75f70c070f826ea89a069a
AinslieSimons <mark@ainsliesimons.com>
2021-09-26 20:39:34 -0500

    ftp updated slightly

--- ftp_services.py
+++ ftp_services.py
@@ -5,9 +5,16 @@ ftp_host = '162.241.253.72'
 ftp_user = 'ons@ainslierockwell.com'
 ftp_pass = 'u(f}rwY1^glh'
 
-ftp = FTP(ftp_host)
-ftp.login(user=ftp_user, passwd=ftp_pass)
-ftp.retrlines('LIST')
+def ftp_connect():
+    ftp = FTP(ftp_host)
+    ftp.login(user=ftp_user, passwd=ftp_pass)
+    print(ftp.getwelcome())
+    print('connected to host: ' + ftp_host)
+    print('logged in as user: ' + ftp_user)
+    ftp.dir()
+    print('you are in directory: ' + ftp.pwd())
 
-with open('readme.txt', 'wb') as fp:
-    ftp.retrbinary('RETR readme.txt', fp.write)
+ftp_connect()
+
+#with open('readme.txt', 'wb') as fp:
+#    ftp.retrbinary('RETR readme.txt', fp.write)
