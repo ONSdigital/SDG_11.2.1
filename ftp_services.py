8d9d8c895cea9a604a42cfb11ec3080f8eaa9170
Mark Simons <mark_mbp_home@mark-macbook-pro.local>
2021-10-08 21:11:09 -0500

    tidying up ftp_services:

--- ftp_services.py
+++ ftp_services.py
@@ -13,22 +13,6 @@ ftp = FTP(ftp_host)
 local_data_dir = "data/"
 remote_data_dir = "data/"
 
-#file_list = ["KS101EW-usual_resident_population.csv",
-#             "Output_Areas__December_2011__Boundaries_EW_BGC.csv",
-#             "RUC11_OA11_EW.csv",
-#             "nomis_QS104EW.csv",
-#             "nomis_QS303.csv"]
-#
-#             ["Lower_Layer_Super_Output_Areas__December_2011__Boundaries_EW_BGC.cpg",
-#             "Lower_Layer_Super_Output_Areas__December_2011__Boundaries_EW_BGC.dbf",
-#             "Lower_Layer_Super_Output_Areas__December_2011__Boundaries_EW_BGC.prj",
-#             "Lower_Layer_Super_Output_Areas__December_2011__Boundaries_EW_BGC.shp",
-#             "Lower_Layer_Super_Output_Areas__December_2011__Boundaries_EW_BGC.shx",
-#             "Lower_Layer_Super_Output_Areas__December_2011__Boundaries_EW_BGC.xml"]
-#             
-#             ["population_estimates/westmids_pop_only.csv"]
-#
-
 file_dict = {"data/" : ["KS101EW-usual_resident_population.csv",
              "Output_Areas__December_2011__Boundaries_EW_BGC.csv",
              "RUC11_OA11_EW.csv",
@@ -65,36 +49,30 @@ def ftp_connect():
     #print('you are in directory: ' + ftp.pwd())
 
 def ftp_get_file(file_to_get):
-    #ftp_connect()
-    print("attempting to retrive:")
-    print(file_to_get)
+    print(f"Transferring: {file_to_get} ")
     with open(file_to_get, 'wb') as fp:
         ftp.retrbinary('RETR ' + file_to_get, fp.write)
-    #ftp.quit()
 
 def dict_iter(input_dict):
     ftp_connect()
-    for remote_path, file_list in file_dict.items():
+    for remote_path, file_list in input_dict.items():
         ftp_get_directory(remote_path, file_list)
-        print(f"transfer of files in {remote_path} completed" )
-    
+        print(f"Completed transfer of files from remote directory {remote_path}\n" )
     ftp.quit()
 
 def ftp_get_directory(remote_data_dir, file_list):
-    print("copying files to: " + os.getcwd())
+    print(f"\nTransfer will deposit files to: {os.getcwd()}/{remote_data_dir}")
     home_dir = os.getcwd()
     ftp_home = ftp.pwd()
     if not os.path.isdir(remote_data_dir): 
         os.mkdir(remote_data_dir)
     os.chdir(remote_data_dir)
     ftp.cwd(remote_data_dir)
-    print(f"currently in remote dir: {ftp.pwd()}")
-    print(ftp.dir())
     for f in file_list:
         ftp_get_file(f)
     ftp.cwd(ftp_home)
     os.chdir(home_dir)
-    print(os.listdir())
+    
 
 def ftp_send_file(file_to_send):
     ftp_connect()
