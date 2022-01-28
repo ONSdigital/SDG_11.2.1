7c516a79003ca30d54e0f56574b57819a94aeba6
james-westwood <mrjameswestwood@gmail.com>
2022-01-28 09:05:07 +0000

    Clearer message showing local auth being processed

--- SDG_11.2.1_main.py
+++ SDG_11.2.1_main.py
@@ -148,7 +148,7 @@ disab_df_dict={}
 age_df_dict={}
 
 for local_auth in list_local_auth:
-    print(local_auth)
+    print(f"Processing: {local_auth}")
     # Get a polygon of la based on the Location Code
     la_poly = (gs.get_polygons_of_loccode(
                             geo_df=uk_LSOA_df,
