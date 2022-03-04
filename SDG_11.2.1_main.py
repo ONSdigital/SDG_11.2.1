ed91d1e3b23110518e427013c6de7717718b069e
James Westwood <mrjameswestwood@gmail.com>
2022-03-04 20:39:58 +0000

    Tidying up a bit

--- SDG_11.2.1_main.py
+++ SDG_11.2.1_main.py
@@ -392,7 +392,7 @@ for local_auth in list_local_auth:
     # Create an LA column 
     disab_servd_df["LA"]=local_auth
 
-    ""==========Disability Disaggregation==========="""
+    """==========Disability Disaggregation==========="""
 
     # Output this iteration's age df to the dict
     disab_df_dict[local_auth]=disab_servd_df
@@ -442,7 +442,7 @@ for local_auth in list_local_auth:
     urb_servd_df["LA"]=local_auth
     rur_servd_df["LA"]=local_auth
 
-   """==========Disability Disaggregation==========="""
+    """==========Disability Disaggregation==========="""
 
     # Output this iteration's urb and rur df to the dict
     urb_rur_df_dict[local_auth]=urb_rur_servd_df_out
