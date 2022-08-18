cb6951fb16d3b0d732854b7bce73436d03a75ccc
james-westwood <mrjameswestwood@gmail.com>
2022-08-18 11:03:56 +0100

    Just ran a linter

--- SDG_bus_timetable.py
+++ SDG_bus_timetable.py
@@ -1,12 +1,12 @@
 # core
 import os
-import zipfile 
+import zipfile
 
 # third party
 import yaml
 import pandas as pd
 
-# our modules 
+# our modules
 import data_ingest as di
 
 # get current working directory
@@ -33,22 +33,22 @@ if not os.path.exists(output_directory):
 # Download bus timetable data
 # Using individual data ingest functions (rather than import_extract_delete_zip)
 # as files are .txt not .csv.
-di._grab_zip(file_nm = bus_dataset_name,
-             zip_link = bus_timetable_zip_link,
-             zip_path = zip_path)
+di._grab_zip(file_nm=bus_dataset_name,
+             zip_link=bus_timetable_zip_link,
+             zip_path=zip_path)
 
 # Extract the required files
 for file in required_files:
     file_extension_name = f"{file}.txt"
 
-    di._extract_zip(file_nm = bus_dataset_name,
-                    csv_nm = file_extension_name,
-                    zip_path = zip_path,
-                    csv_path = output_directory)
+    di._extract_zip(file_nm=bus_dataset_name,
+                    csv_nm=file_extension_name,
+                    zip_path=zip_path,
+                    csv_path=output_directory)
 
 # Remove zip file
-di._delete_junk(file_nm = bus_dataset_name,
-                zip_path = zip_path)
+di._delete_junk(file_nm=bus_dataset_name,
+                zip_path=zip_path)
 
 # Load the text files into pandas dataframes
 # Specifying dtypes to remove unwanted columns and allow loading due to
@@ -60,24 +60,28 @@ di._delete_junk(file_nm = bus_dataset_name,
 # can be done from a string.
 
 # stop times
-stop_times_types = {'trip_id' : 'object', 'departure_time' : 'object', 'stop_id' : 'object'}
+stop_times_types = {'trip_id': 'object',
+                    'departure_time': 'object', 'stop_id': 'object'}
 
-stop_times_df = di._csv_to_df(file_nm = 'stop_times',
-                              csv_path = os.path.join(output_directory, 'stop_times.txt'),
-                              dtypes = stop_times_types)
+stop_times_df = di._csv_to_df(file_nm='stop_times',
+                              csv_path=os.path.join(
+                                  output_directory, 'stop_times.txt'),
+                              dtypes=stop_times_types)
 
 # trips
-trips_types = {'route_id' : 'object', 'service_id' : 'int64', 'trip_id' : 'object'}
+trips_types = {'route_id': 'object',
+               'service_id': 'int64', 'trip_id': 'object'}
 
-trips_df = di._csv_to_df(file_nm = 'trips',
-                         csv_path = os.path.join(output_directory, 'trips.txt'),
-                         dtypes = trips_types)
+trips_df = di._csv_to_df(file_nm='trips',
+                         csv_path=os.path.join(output_directory, 'trips.txt'),
+                         dtypes=trips_types)
 
 # calendar
-calendar_types = {'service_id' : 'int64', 'monday' : 'int64', 'tuesday' : 'int64',
-                 'wednesday' : 'int64', 'thursday' : 'int64', 'friday' : 'int64',
-                 'start_date' : 'object', 'end_date' : 'object'}
+calendar_types = {'service_id': 'int64', 'monday': 'int64', 'tuesday': 'int64',
+                  'wednesday': 'int64', 'thursday': 'int64', 'friday': 'int64',
+                  'start_date': 'object', 'end_date': 'object'}
 
-calendar_df = di._csv_to_df(file_nm = 'calendar',
-                            csv_path = os.path.join(output_directory, 'calendar.txt'),
-                            dtypes = calendar_types)
+calendar_df = di._csv_to_df(file_nm='calendar',
+                            csv_path=os.path.join(
+                                output_directory, 'calendar.txt'),
+                            dtypes=calendar_types)
