5772cc33ba5809c18ff00e5ca7e1d4955bf21abb
nkshaw23 <nathan.shaw@ons.gov.uk>
2022-09-02 10:59:00 +0100

    tidy for PR

--- SDG_train_timetable.py
+++ SDG_train_timetable.py
@@ -24,12 +24,12 @@ train_timetable_zip_link = config["ENG_train_timetable_data"]
 train_dataset_name = 'trains' # changes each time so using holding name
 output_directory = os.path.join(CWD, 'data', 'england_train_timetable')
 zip_path = os.path.join(output_directory, train_dataset_name)
-required_files = ['stop_times', 'trips', 'calendar']
 
 # Download link for zip
 # Name of zip changes each day so will need to scrape the page to extract
 # the latest
-# As such, providing hard-coding filepaths for the time being
+# As such, providing hard-coding filepaths for the time being, and
+# a shortened mca file just for proof of concept.
 msn_file = os.path.join(output_directory, 'ttisf467.msn')
 mca_file = os.path.join(output_directory, 'ttisf467_short.mca')
 
@@ -67,6 +67,8 @@ with open(msn_file, 'r') as msn_data:
         # but without the 100km square (two letters at the start) so
         # very difficult to extract coordinates. Hence, will add in
         # coordinate data from an external source.
+
+        # NB tiploc_code is unique, but crs_code isnt.
         if line.startswith('A'):
             station_name = line[5:31].strip()
             tiploc_code = line[36:43].strip()
@@ -77,63 +79,49 @@ with open(msn_file, 'r') as msn_data:
             msn_df = pd.concat([msn_df, new_df], ignore_index=True)
 
 
-# Data contains alot more records than other sources of station names
-# Also contains duplicates in the crs_code
-# However tiploc_code is unique
-msn_tiploc_unique = msn_df['tiploc_code'].is_unique
-msn_crs_unique = msn_df['crs_code'].is_unique
-
-# Think it makes sense to remove the duplicates in crs_code
+# Remove the duplicates in crs_code
 msn_df = msn_df.drop_duplicates(subset = ['crs_code'])
 
-# Attache the coordinates from the json
-# At the moment using the original data from the github repo (linked in the issue)
-# https://github.com/ellcom/UK-Train-Station-Locations/blob/master/uk-train-stations-dictonary.json
-# James has reviewed and made amendments to this, so can use the latest when
-# available.
-
-# NOTE: Will need adding to cloud storage if we keep this method of
-# attaching coordinates.
+# Attach the coordinates for each train station
 station_locations = os.path.join(output_directory, 'station_locations.csv')
 station_locations_df = pd.read_csv(station_locations, usecols = ['station_code', 'latitude', 'longitude'])
 
 # Join coordinates onto msn data
-
 # left join to master station names and see which ones dont have lat and long
 msn_data = pd.merge(msn_df, station_locations_df, how = 'left',
                         left_on = 'crs_code', right_on='station_code')
 msn_data = msn_data[['station_name', 'tiploc_code', 'crs_code', 'latitude', 'longitude']]
 
-# There are quite a few empty coords as the msn data contained alot
-# more records.
-no_coords = msn_data[msn_data['latitude'].isna()]
-
-# Remove these from the data for the time being
+# Remove stations with no coordinates
 msn_data = msn_data.dropna(subset=['latitude', 'longitude'], how='any')
 
 
 # Extract mca data
 #-----------------
 
-# Basic idea which is massively simplified. Need to double check we are not
-# omitting anything really important.
 # Each new journey starts with BS. Within this journey we have
 # multiple stops
 # LO is origin
 # LI are inbetween stops
 # LT is terminating stop
-# Then a new jounrey starts with BS again
-# Within each journey are a few more lines that I beleieve we can ignore
-# BX = extra details of the jounrey
+# Then a new journey starts with BS again
+# Within each journey are a few more lines that we can ignore e.g.
+# BX = extra details of the journey
 # CR = changes en route. Doesnt contain any arrival / departure times.
 
 # Create dataframe with specified schema
 mca_df = pd.DataFrame({'tiploc_code': pd.Series(dtype = 'str'),
-                       'arrival_time': pd.Series(dtype='str'),
-                       'calender': pd.Series(dtype='str')})
+                       'departure_time': pd.Series(dtype='str'),
+                       'calender': pd.Series(dtype='str'),
+                       'start_date': pd.Series(dtype='str'),
+                       'end_date': pd.Series(dtype='str'),
+                       'activity_type': pd.Series(dtype='str')})
 
 # Create a flag that specifies when we have found a new journey
 journey = False
+
+# Create a flag that specifies we have found a departure time
+time = False
 
 with open(mca_file, 'r') as mca_data:
     # Skip the header
@@ -144,11 +132,11 @@ with open(mca_file, 'r') as mca_data:
 
         # Schedules are then further broken down by transaction type 
         # (N - new, R - revised, D - delete)
-        # Remove any that are transaction type delete.
+        # Ignore any that are transaction type delete.
         if line[0:3] == 'BSN' or line[0:3] == 'BSR':
             # Switch flag on as we have found a jounrey
             journey = True
-            # Extract start and end date of service
+            # Extract start and end date of service (yymmdd)
             start_date = line[9:15].strip()
             end_date = line[15:21].strip()
             # Extract the calender information for this journey
@@ -162,28 +150,61 @@ with open(mca_file, 'r') as mca_data:
         # part of this jounrey and extract relevant information.
 
         # Journeys split into origin (LO), stops (LI) and terminus (LT)
-        
         # CR (changes en route) and BX (extra schedule details) can be
         # ignored as not relevant to our purpose.
 
+        # If station has a departure time set the time flag to True
+        # and extract time, tiploc_code and activity type.
+
+        # NB if departure time doesnt exist we are not the trying to extract
+        # from either public scheduled / arrival time or scheduled pass time 
+        # as not relevant.
+
+        # NB times can end on a H sometimes which indicates a half minute
+        # Rather than rounding up and down, just ignoring this for the moment
+        # and taking only first four characters (hh:mm)
+
+        # NB Only keep LI records where the activity_type contains T (stops to
+        # take up and set down passengers). All other activity types unsuitable.
+
+
+
         if journey:
-            if line[0:2] == 'LO' or line[0:2] == 'LI':
-                # Extract tiploc_code
-                tiploc_code = line[2:10].strip()
-                # Extract departure time for journey origin station
+            if line[0:2] == 'LO':
                 if line[10:15].strip() != '':
-                    departure_time = line[10:15]
-                else:
-                    departure_time = line[15:20]
-            elif line[0:2] == 'LT':
-                # Extract tiploc_code
-                tiploc_code = line[2:10].strip()
-                # Extract departure time
+                    time = True
+                    # Extract departure time
+                    departure_time = line[10:14].strip()
+                    # Extract tiploc_code
+                    tiploc_code = line[2:10].strip()
+                    # Extract activity type
+                    activity_type = line[29:41].strip()
+            elif line[0:2] == 'LI':
+                # If station has a departure time set the flag to True
+                # and extract time, tiploc_code and activity type
                 if line[15:20].strip() != '':
-                    departure_time = line[15:20]
-                else:
-                    departure_time = line[29:33]
-                
+                    time = True
+                    # Extract departure time
+                    departure_time = line[15:19].strip()
+                    # Extract tiploc_code
+                    tiploc_code = line[2:10].strip()
+                    # Extract activity type if it contains T
+                    # Activity type made up of upto 6 types
+                    # each 2 characters so search for 'T '
+                    if 'T ' in line[42:54]:
+                        activity_type = line[42:54].strip()
+            elif line[0:2] == 'LT':
+                # If station has a departure time set the flag to True
+                # and extract time, tiploc_code and activity type
+                if line[15:20].strip() != '':
+                    time = True
+                    # Extract departure time
+                    departure_time = line[15:19].strip()
+                    # Extract tiploc_code
+                    tiploc_code = line[2:10].strip()
+                    # Extract acitivity type
+                    activity_type = line[25:37].strip()
+
                 # As we know that LT signifies the last stop in a journey
                 # and we have extracted everything we need we now switch the flag off
                 journey = False
@@ -191,27 +212,13 @@ with open(mca_file, 'r') as mca_data:
                 # Skipping BR and CX
                 continue 
             
-            # Ceate a new data frame for each station in this journey, adding
-            # the calender days into it.
-            new_df = pd.DataFrame([[tiploc_code, departure_time, start_date, end_date, calender]],
-                                    columns = mca_df.keys())
-            mca_df = pd.concat([mca_df, new_df], ignore_index=True)
+            # If time flag has been switched on, then create a new data frame for 
+            # this stop, adding the calender days into it.
+            if time:
+                new_df = pd.DataFrame([[tiploc_code, departure_time,
+                                        calender, start_date, end_date, activity_type]],
+                                        columns = mca_df.keys())
+                mca_df = pd.concat([mca_df, new_df], ignore_index=True)
 
-
-# Filter the MCA data
-# -------------------
-
-# Remove records that dont contain a departure time.
-# We are not trying to add a time from either public scheduled / arrival time
-# or scheduled pass time as not relevant.
-
-
-
-
-# Only keep LI records where activity is T (stops to take up and set down
-# passengers). All other activity types unsuitable.
-# Do group by to see counts for different permutations to understand
-# what we would be losing.
-
-
-# Round up or down where time contains H (which means a half minute)
+            # Reset time flag for new line
+            time = False
