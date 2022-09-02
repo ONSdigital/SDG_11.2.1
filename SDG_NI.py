b0036c8428a7b450771d219d066ac11493253740
Paige Hunter <paige.hunter@ons.gov.uk>
2022-09-02 15:12:47 +0100

    Read in mid-year estimates

--- SDG_NI.py
+++ SDG_NI.py
@@ -17,6 +17,16 @@ with open(os.path.join(CWD, "config.yaml")) as yamlfile:
     module = os.path.basename(__file__)
     print(f"Config loaded in {module}")
 
+# Years
+# Getting the year for population data
+pop_year = str(config["calculation_year"])
+
+# Load config
+with open(os.path.join(CWD, "config.yaml")) as yamlfile:
+    config = yaml.load(yamlfile, Loader=yaml.FullLoader)
+    module = os.path.basename(__file__)
+    print(f"Config loaded in {module}")
+
 # gets the northern ireland bus stops data from the api
 ni_bus_stop_url = config["NI_bus_stops_data"]
 output_ni_bus_csv = os.path.join(CWD,"data","Stops","NI","bus_stops_ni.csv")
@@ -36,12 +46,21 @@ di.get_ni_stops_from_api(url=ni_train_stop_url,
 ni_train_stops = di.read_ni_stops(output_ni_train_csv)
 
 # Get usual population for Northern Ireland
-usual_pop_path = os.path.join(CWD, "data", "KS101NI.csv")
+whole_NI_df = pd.read_csv(os.path.join(CWD, "data", "KS101NI.csv"),
+                             header=2)
+# Only use columns that we need
+cols_NI_df = ["SA Code", "All usual residents","Usual residents: Males","Usual residents: Females"]
+ni_usual_pop = whole_NI_df[cols_NI_df]
 
- # reads in data and crops header
-df = pd.read_csv(usual_pop_path, 
-                header=2)
+# Read in mid-year population estimates for Northern Ireland
+pop_files = pd.read_csv(os.path.join(CWD, 
+                                     "data", "population_estimates", 
+                                     "SAPE20-SA-Totals.csv"),
+                        header=7)
 
-# only use columns that we need
-essential_cols = ["SA Code", "All usual residents","Usual residents: Males","Usual residents: Females"]
-ni_usual_pop = df[essential_cols]
+# Filter to small area code and population year columns only
+estimate_cols = ["Area_Code", pop_year]
+estimate_pop_NI = pop_files[estimate_cols]
+
+
+
