d570dbe45ea385e060d06f1e22e1dc5e06782783
nkshaw23 <nathan.shaw@ons.gov.uk>
2022-09-16 10:13:53 +0100

    refactor existing code

--- SDG_train_timetable.py
+++ SDG_train_timetable.py
@@ -4,6 +4,7 @@ import os
 # third party
 import yaml
 import pandas as pd
+from tqdm import tqdm
 
 # get current working directory
 CWD = os.getcwd()
@@ -17,9 +18,7 @@ with open(os.path.join(CWD, "config.yaml")) as yamlfile:
 
 # Parameters
 train_timetable_zip_link = config["ENG_train_timetable_data"]
-train_dataset_name = 'trains' # changes each time so using holding name
 output_directory = os.path.join(CWD, 'data', 'england_train_timetable')
-zip_path = os.path.join(output_directory, train_dataset_name)
 
 # Download link for zip
 # Name of zip changes each day so will need to scrape the page to extract
@@ -27,10 +26,14 @@ zip_path = os.path.join(output_directory, train_dataset_name)
 # As such, providing hard-coding filepaths for the time being, and
 # a shortened mca file just for proof of concept.
 msn_file = os.path.join(output_directory, 'ttisf467.msn')
-mca_file = os.path.join(output_directory, 'ttisf467_short.mca')
+mca_file = os.path.join(output_directory, 'ttisf467_medium.mca')
 
 
 # Below code can be used once we have the latest filename
+# train_dataset_name = 'trains'
+# changes each time so using holding name
+# zip_path = os.path.join(output_directory, train_dataset_name)
+
 #di._grab_zip(file_nm=train_dataset_name,
 #            zip_link=train_timetable_zip_link,
 #            zip_path=zip_path)
@@ -44,10 +47,8 @@ mca_file = os.path.join(output_directory, 'ttisf467_short.mca')
 # Extract msn data
 #-----------------
 
-# Create dataframe with specified schema
-msn_df = pd.DataFrame({'station_name': pd.Series(dtype = 'str'),
-                       'tiploc_code': pd.Series(dtype='category'),
-                       'crs_code': pd.Series(dtype='category')})
+# Store msn data
+msn = []
 
 with open(msn_file, 'r') as msn_data:
     # Skip header
@@ -70,10 +71,17 @@ with open(msn_file, 'r') as msn_data:
             tiploc_code = line[36:43].strip()
             crs_code = line[49:52].strip()
 
-            # Create a new datafrme to concatenate to the original
-            new_df = pd.DataFrame([[station_name, tiploc_code, crs_code]], columns = msn_df.keys())
-            msn_df = pd.concat([msn_df, new_df], ignore_index=True)
+            msn.append([station_name,
+						tiploc_code,
+						crs_code])
 
+
+# Create dataframe
+msn_df = pd.DataFrame(msn, columns = ['station_name', 'tiploc_code', 'crs_code'])
+
+
+# Clean msn data
+# --------------
 
 # Remove the duplicates in crs_code
 msn_df = msn_df.drop_duplicates(subset = ['crs_code'])
@@ -106,24 +114,23 @@ msn_data = msn_data.dropna(subset=['latitude', 'longitude'], how='any')
 # BX = extra details of the journey
 # CR = changes en route. Doesnt contain any arrival / departure times.
 
-# Create dataframe with specified schema
-mca_df = pd.DataFrame({'tiploc_code': pd.Series(dtype = 'str'),
-                       'departure_time': pd.Series(dtype='str'),
-                       'calender': pd.Series(dtype='str'),
-                       'start_date': pd.Series(dtype='str'),
-                       'end_date': pd.Series(dtype='str'),
-                       'activity_type': pd.Series(dtype='str')})
-
 # Create a flag that specifies when we have found a new journey
 journey = False
 
-# Create a flag that specifies we have found a departure time
-time = False
+# Store schedule information
+schedules = []
+
+# Store stop information
+stops = []
+
+# Start by finding all the schedules within the file. Extract relevant information
+# into the journey dataframe, and then copy unique_id onto all trips
+# within that journey.
 
 with open(mca_file, 'r') as mca_data:
     # Skip the header
     next(mca_data)
-    for line in mca_data:
+    for line in tqdm(mca_data):
         # A schedule is started by a record beginning with BS.
         # Other entries exist but are not needed for our purpose.
 
@@ -131,76 +138,73 @@ with open(mca_file, 'r') as mca_data:
         # (N - new, R - revised, D - delete)
         # Ignore any that are transaction type delete.
         if line[0:3] == 'BSN' or line[0:3] == 'BSR':
-            # Switch flag on as we have found a jounrey
+            # Switch flag on as we have found a journey
             journey = True
+			
+			# Get unique ID for schedule
+            # ID in dataset is not actually unique as same train has several schedules
+            # with different dates and calendars. Create ID from these variables.
+            schedule_id = line[3:9] + line[9:15] + line[15:21] + line[21:28]
+			
             # Extract start and end date of service (yymmdd)
             start_date = line[9:15].strip()
             end_date = line[15:21].strip()
+			
             # Extract the calender information for this journey
             # i.e. what days of the week it runs
-            calender = line[21:28].strip()
+            # Only interested in weekdays for the timebeing.
+            mon = line[21].strip()
+            tues = line[22].strip()
+            wed = line[23].strip()
+            thurs = line[24].strip()
+            fri = line[25].strip()
+			
+			# Store data to be added to the dataframe
+            schedules.append([schedule_id,
+                            start_date,
+							end_date,
+							mon,
+							tues,
+							wed,
+							thurs,
+							fri])
+			
             # Skip as this is all we need to do for an entry starting
             # with BS
             continue
-
+		
         # If we have found a new journey, go through the lines that are
-        # part of this jounrey and extract relevant information.
+        # part of this jounrey and give them the schedule id.
 
         # Journeys split into origin (LO), stops (LI) and terminus (LT)
         # CR (changes en route) and BX (extra schedule details) can be
         # ignored as not relevant to our purpose.
 
-        # If station has a departure time set the time flag to True
-        # and extract time, tiploc_code and activity type.
-
-        # NB if departure time doesnt exist we are not the trying to extract
-        # from either public scheduled / arrival time or scheduled pass time
-        # as not relevant.
+        # If station has a departure time extract time, tiploc_code and activity type.
 
         # NB times can end on a H sometimes which indicates a half minute
         # Rather than rounding up and down, just ignoring this for the moment
         # and taking only first four characters (hh:mm)
 
-        # NB Only keep LI records where the activity_type contains T (stops to
-        # take up and set down passengers). All other activity types unsuitable.
-
-
-
         if journey:
+			
             if line[0:2] == 'LO':
-                if line[10:15].strip() != '':
-                    time = True
-                    # Extract departure time
-                    departure_time = line[10:14].strip()
-                    # Extract tiploc_code
-                    tiploc_code = line[2:10].strip()
-                    # Extract activity type
-                    activity_type = line[29:41].strip()
+
+                departure_time = line[10:14].strip()
+                tiploc_code = line[2:10].strip()
+                activity_type = line[29:41].strip()
+				
             elif line[0:2] == 'LI':
-                # If station has a departure time set the flag to True
-                # and extract time, tiploc_code and activity type
-                if line[15:20].strip() != '':
-                    time = True
-                    # Extract departure time
-                    departure_time = line[15:19].strip()
-                    # Extract tiploc_code
-                    tiploc_code = line[2:10].strip()
-                    # Extract activity type if it contains T
-                    # Activity type made up of upto 6 types
-                    # each 2 characters so search for 'T '
-                    if 'T ' in line[42:54]:
-                        activity_type = line[42:54].strip()
+                    
+                departure_time = line[15:19].strip()
+                tiploc_code = line[2:10].strip()
+                activity_type = line[42:54].strip()
+						
             elif line[0:2] == 'LT':
-                # If station has a departure time set the flag to True
-                # and extract time, tiploc_code and activity type
-                if line[15:20].strip() != '':
-                    time = True
-                    # Extract departure time
-                    departure_time = line[15:19].strip()
-                    # Extract tiploc_code
-                    tiploc_code = line[2:10].strip()
-                    # Extract acitivity type
-                    activity_type = line[25:37].strip()
+
+                departure_time = line[15:19].strip()
+                tiploc_code = line[2:10].strip()
+                activity_type = line[25:37].strip()
 
                 # As we know that LT signifies the last stop in a journey
                 # and we have extracted everything we need we now switch the flag off
@@ -209,13 +213,46 @@ with open(mca_file, 'r') as mca_data:
                 # Skipping BR and CX
                 continue
 
-            # If time flag has been switched on, then create a new data frame for
-            # this stop, adding the calender days into it.
-            if time:
-                new_df = pd.DataFrame([[tiploc_code, departure_time,
-                                        calender, start_date, end_date, activity_type]],
-                                        columns = mca_df.keys())
-                mca_df = pd.concat([mca_df, new_df], ignore_index=True)
+			# Store data to be added to dataframe
+            stops.append([schedule_id,
+						departure_time,
+						tiploc_code,
+						activity_type])
+								
+								
+# Add data into dataframes
+mca_schedule_df = pd.DataFrame(schedules,
+							   columns=['schedule_id',
+                                        'start_date',
+										'end_date',
+										'mon',
+										'tues',
+										'wed',
+										'thurs',
+										'fri'])
 
-            # Reset time flag for new line
-            time = False
+		
+mca_stop_df = pd.DataFrame(stops,
+						  columns = ['schedule_id',
+                                    'departure_time',
+									'tiploc_code',
+									'activity_type'])
+							
+							
+									
+			
+# Clean mca data
+# --------------
+# check schdule id unique in df!!!
+# schedule not unique as some have same id but with different date and different times???
+
+# 1 remove empty times. (use strip != '')
+# 2 Extract only activity type T. 
+# NB Only keep LI records where the activity_type contains T (stops to
+# take up and set down passengers). All other activity types unsuitable.
+# Extract activity type if it contains T
+# Activity type made up of upto 6 types
+# each 2 characters so search for 'T '
+
+
+# Filter schdules by day and then use the unique id to extract stops from stop df.
