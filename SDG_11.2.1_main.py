6b1072f3edfd5ab87d8105f0c8d7e893136ef3a3
James Westwood <mrjameswestwood@gmail.com>
2022-12-16 14:55:51 +0000

    importing bus_highly_serviced_stops

--- SDG_11.2.1_main.py
+++ SDG_11.2.1_main.py
@@ -52,7 +52,7 @@ CENTROID_YEAR = str(config["centroid_year"])
 # Metros and trains added from NAPTAN as we dont have timetable
 # data for these stops. Hence, they wont be highly serviced.
 
-highly_serviced_bus_stops = di._feath_to_df('highly_serviced_stops',
+highly_serviced_bus_stops = di._feath_to_df('bus_highly_serviced_stops',
                                             BUS_IN_DIR)
 highly_serviced_train_stops = di._feath_to_df('train_highly_serviced_stops',
                                               TRAIN_IN_DIR)
