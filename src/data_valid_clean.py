79744f864ebb4dfd50352d00005aa4331ff73c2b
jwestw <james.westwood@ons.gov.uk>
2023-06-09 17:30:30 +0000

    Better function for validation

--- src/data_valid_clean.py
+++ src/data_valid_clean.py
@@ -1,26 +1,15 @@
 """All functions related to data validation and cleaning."""
-
+import re
 
 def uppercase_column_names(df):
     df.columns = df.columns.str.upper()
     df.rename(columns={'GEOMETRY': 'geometry'}, inplace=True)
 
 
-def check_df_col_names(df, correct_cols, regex_cols=None):
-    """Check that the column names of a dataframe are correct.
-        Will accept a list of column names in any order.
-        Any column can be a regex pattern.
-
-    Args:
-        df (pd.DataFrame): Dataframe to check.
-        correct_cols (list): List of correct column names.
-
-    Returns:
-        bool: True if the column names are correct, False otherwise.
-    """
-    
-    if regex_cols:
-        for col in regex_cols:
-            
-    
-    return list(df.columns) == correct_cols
+def check_required_columns(dataframe, required_columns):
+    missing_columns = []
+    for column in required_columns:
+        if column not in dataframe.columns:
+            missing_columns.append(column)
+    if missing_columns:
+        raise ValueError(f"The following columns are missing: {missing_columns}")
