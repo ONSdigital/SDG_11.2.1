072b6e9d37fc57cab82e48374e1e16b5ce65d7e8
jwestw <james.westwood@ons.gov.uk>
2023-07-07 12:07:08 +0000

    Testing new method

--- src/pre_processing/pop_estimates_archive_pre_process.py
+++ src/pre_processing/pop_estimates_archive_pre_process.py
@@ -242,5 +242,5 @@ def main():
     con.close()
 
 if __name__ == "__main__":
-    """This is executed when run from the command line."""
+    """This is executed when run from the command line!"""
     main()
