eda6f06ed7d97b9f3bd5ca637405e3c2121e0a85
jwestw <james.westwood@ons.gov.uk>
2023-08-18 14:24:10 +0000

    A major DuckDB based re-write

--- src/pre_processing/pop_estimates_archive_pre_process.py
+++ src/pre_processing/pop_estimates_archive_pre_process.py
@@ -8,108 +8,24 @@ import pandas as pd
 import sys
 from glob import glob
 import duckdb
+import uuid
+from typing import List
 
-# %%
-db_output = "data/population_estimates/2002-2012/pop_est_2002-2012.db"
+db_file_path = "data/population_estimates/2002-2012/pop_est_2002-2012.db"
 
+# Define the input and output file paths
+input_folder = "data/population_estimates/2002-2012"
 
-def extract_region(file_name):
-    # Extact the region name from the file name
-    region_pattern = r"unformatted-(.*?)-mid2002"
-    match = re.search(region_pattern, file_name)
+# Define the years to process
+years = list(range(2002, 2013))
 
-    if match:
-        region = match.group(1)
+# Define an empty dictionary to store the dataframes for each year
+year_data = {}
 
-    return region
+# Define the column name for the year
+year_cols = [f"Population_{year}" for year in years]
 
-
-def pivot_year_dfs(year_col, *year_dfs):
-    """This function pivots the dataframes to a wide format and
-        adds a All Ages column to each dataframe which sums the population
-        for all ages across each OA"""
-    pivoted_dfs = []
-    for year_df in year_dfs:
-        pivoted_df = year_df.pivot_table(index=["OA11CD", "LAD11CD"], columns=[
-                                         "Age"], values=year_col, aggfunc="sum")
-        pivoted_df["All Ages"] = pivoted_df.sum(axis=1)
-        pivoted_dfs.append(pivoted_df)
-    return tuple(pivoted_dfs)
-
-
-def make_feather_path(year, output_folder, name):
-    file_path = pl.Path.joinpath(output_folder, f"pop_est_{name}_all_regions_{year}.feather")
-    return file_path
-
-
-def age_pop_one_year(combined_sexes_df, year):
-    """Splits the data into three sex groups and drops the sex column.
-    
-    This function takes a long (not wide) dataframe and returns three dataframes
-    long dataframes. 
-    """
-
-    cols = ["OA11CD", "Age", "LAD11CD", f"Population_{year}"]
-
-    # get single sex dfs
-    both_sexes_df = combined_sexes_df[combined_sexes_df.Sex == 1 | 2][cols]
-    male_df = combined_sexes_df[combined_sexes_df.Sex == 1][cols]
-    female_df = combined_sexes_df[combined_sexes_df.Sex == 2][cols]
-
-    return both_sexes_df, male_df, female_df
-
-def load_region_data(file_path, year_cols):
-    """Load the data for a single region and return a dataframe."""
-        
-    # load all years for one region
-    single_region_df = pd.read_csv(file_path, usecols=["OA11CD",
-                                                        "LAD11CD",
-                                                        "Age",
-                                                        "Sex",
-                                                        *year_cols])
-    region_code = extract_region(file_path)
-    
-    # Create a column called region
-    single_region_df["region"] = region_code
-    
-    # Reorder the columns
-    ordered_cols = ["OA11CD", "LAD11CD", "Age", "Sex", "region"]+year_cols
-    single_region_df = single_region_df[ordered_cols]
-    
-    return single_region_df
-
-
-def create_output_folder(year: int) -> pl.Path:
-    """Create the output folder for a given year if it doesn't exist."""
-    output_folder = pl.Path(f"data/population_estimates/{year}")
-    if not output_folder.exists():
-        output_folder.mkdir(parents=True)
-    return output_folder
-
-
-def stringify_column_names(df):
-    """Stringify the column names of a DataFrame."""
-    df.rename(columns={col: str(col) for col in df.columns}, inplace=True)
-
-# %%
-def main():
-
-    # Define the input and output file paths
-    input_folder = "data/population_estimates/2002-2012"
-
-    # Define the years to process
-    years = list(range(2002, 2013))
-
-    # Define an empty dictionary to store the dataframes for each year
-    year_data = {}
-
-    # Loop through each year and process the data for all regions
-
-
-    # Define the column name for the year
-    year_cols = [f"Population_{year}" for year in years]
-    
-    column_types = {
+column_types = {
     "OA11CD": "TEXT",
     "LAD11CD": "TEXT",
     "Age": "TEXT",
@@ -126,72 +42,202 @@ def main():
     "Population_2011": "INTEGER",
     "Population_2012": "INTEGER"
 }
-    
-    # Create a connection to the database
-    con = duckdb.connect(db_output)
 
-    # Create a query to load all the csv data in one go
-    query = f"""
-    CREATE TABLE IF NOT EXISTS all_csvs
-    AS SELECT * 
-    FROM read_csv_auto('data/population_estimates/2002-2012/*.csv',
-    header=true,
-    columns={column_types})"""
+
+def create_connection(database_path):
+    """Creates a connection to a DuckDB database and returns the connection object."""
+    con = duckdb.connect(database_path)
+    return con
+
+
+def load_all_csvs(con, csv_folder, output_table_name):
+    """Loads all the population csv files in a folder into a DuckDB database."""
+    
+    load_csv_query = """
+    CREATE TABLE IF NOT EXISTS {output_table_name}
+    AS SELECT *
+    FROM read_csv_auto('{csv_folder}/*', header=true, columns={column_types});
+    """
+    
+    con.execute(load_csv_query)
+    
+    return output_table_name
+
+def query_database(con, query):
+    """Queries a DuckDB database and returns the name of a temporary table.
+    
+    This function takes a DuckDB connection object and a SQL query as input,
+    and creates a temporary tcable with the results of the query. The function
+    returns the *name* of the temporary table, which can be passed to the next function.
+    """
+    # Generate a unique name for the temporary table
+    table_name = f"temp_{uuid.uuid4().hex}"
+
+    # Create the temporary table and insert the query results
+    con.execute(f"CREATE TEMPORARY TABLE {table_name} AS {query};")
+
+    # Return the name of the temporary table
+    return table_name
+
+def query_database_as_view(con, query, view_name):
+    """Queries a DuckDB database and creates or updates a view with the results.
+    
+    This function takes a DuckDB connection object, a SQL query, and a view name as input,
+    and creates or updates a view with the results of the query. The function returns the view object,
+    which can be used in subsequent queries.
+    """
+    # Create or update the view with the results of the query
+    con.execute(f"CREATE OR REPLACE VIEW {view_name} AS {query};")
+
+    # Return the updated view object
+    return con.view(view_name)
+
+def extract_region(file_name):
+    # Extact the region name from the file name
+    region_pattern = r"unformatted-(.*?)-mid2002"
+    match = re.search(region_pattern, file_name)
+
+    if match:
+        region = match.group(1)
+
+    return region
+
+
+def pivot_sex_tables(con, male_table: str, female_table: str, both_table: str, year_col: str):
+    """Pivots the sex-specific tables using SQL.
+    
+    Args:
+        con: A DuckDB connection object.
+        male_table: A string representing the name of the male sex-specific table.
+        female_table: A string representing the name of the female sex-specific table.
+        both_table: A string representing the name of the both sex-specific table.
+        year_col: A string representing the name of the column containing the population data.
+        
+    Returns:
+        Three dataframes representing the pivoted male, female, and both sex-specific tables.
+    """
+    
+    # Construct the SQL query to pivot the male sex-specific table
+    male_query = f"""
+        SELECT OA11CD, LAD11CD, {', '.join([f"SUM(CASE WHEN Age = {i} THEN {year_col} ELSE 0 END) AS Age_{i}" for i in range(0, 101)])}
+        FROM {male_table}
+        GROUP BY OA11CD, LAD11CD;"""
+    
+    # Construct the SQL query to pivot the female sex-specific table
+    female_query = f"""
+        SELECT OA11CD, LAD11CD, {', '.join([f"SUM(CASE WHEN Age = {i} THEN {year_col} ELSE 0 END) AS Age_{i}" for i in range(0, 101)])}
+        FROM {female_table}
+        GROUP BY OA11CD, LAD11CD;"""
+    
+    # Construct the SQL query to pivot the both sex-specific table
+    both_query = f"""
+        SELECT OA11CD, LAD11CD, {', '.join([f"SUM(CASE WHEN Age = {i} THEN {year_col} ELSE 0 END) AS Age_{i}" for i in range(0, 101)])}
+        FROM {both_table}
+        GROUP BY OA11CD, LAD11CD;"""
+    
+    # Execute the SQL queries and get dataframes for each sex-specific table
+    male_df = query_database(con, male_query)
+    female_df = query_database(con, female_query)
+    both_df = query_database(con, both_query)
+    
+    return male_df, female_df, both_df
+
+
+def age_pop_by_sex(con: duckdb.DuckDBPyConnection, table_name, year: int):
+    """"Uses SQL to get the data for three sex groups and drops the sex column.
+    
+    Args:
+        con: A DuckDB connection object.
+        table_name: The name of the table which corresponds to one year of data
+        year: An integer representing the year to query.
+        sex_num: 
+    """
+    
+    # Construct the SQL query for the male sex group
+    male_query = f"""
+        SELECT OA11CD, Age, LAD11CD, Population_{year}
+        FROM {table_name}
+        WHERE sex = 1;"""
+
+    # Construct the SQL query for the female sex group
+    female_query = f"""
+        SELECT OA11CD, Age, LAD11CD, Population_{year}
+        FROM {table_name}
+        WHERE sex = 2;"""
+        
+    # Construct the SQL query for both sex groups
+    both_query = """
+        SELECT OA11CD, Age, LAD11CD, Population_2002
+        FROM {table_name};"""
+    
+    # Get a dataframe for sex == 1
+    male_table = query_database(con, male_query)
+    
+    # Get a dataframe for sex == 2, Female
+    female_table = query_database(con, female_query)
+
+    # Get combined sexes dataframe
+    both_table = query_database(con, both_query)
+    
+    return male_table, female_table, both_table
+
+  
+    
+def create_output_folder(year: int) -> pl.Path:
+    """Create the output folder for a given year if it doesn't exist."""
+    output_folder = pl.Path(f"data/population_estimates/{year}")
+    if not output_folder.exists():
+        output_folder.mkdir(parents=True)
+    return output_folder
+
+
+def write_table_to_csv(con, *args, output_folder: pl.Path, year: int):
+    
+    for table_name in args:
+        query=f"""
+        SELECT * FROM {table_name}
+        COPY TO '{output_folder}/{table_name}_{year}.csv' (HEADER TRUE, DELIMITER ',');"""
+        
+        query_database(con, query)
+    
+    return None
+    
+def main():
+
+    # Create connection 
+    con = create_connection(db_file_path)
     
     # Run query to load all the csv data in one go and create the table
-    con.execute(query)
-    
+    table_name = load_all_csvs(con, input_folder)
 
 
-
-
-    # Get the data for all regions for one year using year_cols[0]
-    # Single year_data cols
+    # For each of those years, load the data for all regions into a temp table
+    # and return the name of the temp table
+    temp_table_names = []
     for year in years:
         
-        year_col = f"Population_{year}"
+        year_col=f"Population_{year}"
+           
         
-        single_yr_cols = ["OA11CD", "LAD11CD", "Age", "Sex", year_col]
-        columns = ", ".join(single_yr_cols)
-        all_data_for_one_year = con.execute(f"SELECT {columns} FROM all_csvs LIMIT 10").df()
-        
-        ##### THIS IS WHERE I GOT TO #####
-        
-        # TODO: create a duckdb version of the following functions including filtering by sex
-        # and pivoting the data to a wide format
-        # and writing to a feather file
-        
-                
-        # Get the three sex disaggregated dataframes
-        # Call the function to get the dataframes
-        both_sexes_df, male_df, female_df = age_pop_one_year(all_data_for_one_year, year)
+        # Get the three sex disaggregated tables
+        both_sexes_table, male_table, female_table = age_pop_by_sex(con, table_name, year)
 
+        # Pack the three names as a tuple
+        all_three_tables = both_sexes_table, male_table, female_table
+        
         # Pivot the dataframe to a wide format
-        # grouped_df = df.groupby(["OA11CD", "Age"]).sum().pivot_table(index="OA11CD", columns="Age", values="Population_2002")
-
-        both_sexes_df, male_df, female_df = pivot_year_dfs(
-            year_col, both_sexes_df, male_df, female_df)
-
-        # Create a dictionary with the dataframes
-        data_by_sex = {"both_sexes_df": both_sexes_df,
-                        "male_df": male_df,
-                        "female_df": female_df}
-
-        output_folder= create_output_folder(year)
+        all_three_tables = pivot_sex_tables(con, *all_three_tables, year_col)
+        
+        # Create the folder for the year, and return its path
+        output_folder = create_output_folder(year)
+        
+        # Write out the pivoted tables as CSV files
+        write_table_to_csv(con, *all_three_tables,
+                           output_folder=output_folder,
+                           year=year)
     
-    # Write year_df to a feather file
-    for name, sex_in_one_year_df in data_by_sex.items():
-        # Stringify the column names (because feather doesn't like ints as column names)
-        stringify_column_names(sex_in_one_year_df)
+    # Close the connection
+    con.close()
         
-        # Write the dataframe to a feather file
-        file_path = make_feather_path(year, output_folder, name)
-        
-        try:
-            sex_in_one_year_df.reset_index().to_feather(file_path)
-        except ValueError or FileNotFoundError as e:
-            print(e)
-            sys.exit(1)
-
 if __name__ == "__main__":
     main()
